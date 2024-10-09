ffaa8ff0d680c234eece2521154b92f2571d6f0d
Isaac Ramos <isaacantonioramosperez@alumno.ieselrincon.es>
2024-10-09 20:38:15 +0100

    añadido actividades 6,7,8

--- s_first/t1/DEW24-25/Activity 3/main.js
+++ s_first/t1/DEW24-25/Activity 3/main.js
@@ -4,23 +4,55 @@
 // arg -> 1 - url
 // arg -> 2 - name
 // arg -> 3 - size
-function ej1 (){
+function ej1(){
   window.open('', '','width=200, height=100');
 }
-function ej2 (){
+
+function ej2(){
   window.open('', 'MsgWindow','');
   window.document.write('hey Socio');
 }
 
-function ej3 (){
-  window.open('', 'MsgWindows', '');
+function ej3(){
+  window.open('', 'MsgWindows', ''); 
   window.document.write('some other text');
-
 }
-function ej4 (){
-  window.location.href("about:blanck") = 'www.google.es'
-}
-function ej5 (){
-  window.open('', '', 'toolbar=yes, scrollbars=yes, resizable=yes, width=200, height=400, left=200, top=100');
 
+// miriam
+function ej3_miriam(){
+  window.document.location.replace('https://www.google.com');
+}
+function ej6(){
+  let window = window.open('https://www.google.com');
+  window.close;
+}
+// miraim
+function ej6_miriam(){
+  let tempWindow = window.open('','tempoWindow','');
+  setTimeout(() => {
+    tempWindow.close;
+  }, 2000);
+}
+function ej7(){
+  let openerWindow = window.open('', '', '');
+  openerWindow.opener.document.write('some window text');
+}
+function ej8(){
+  let iframe = document.createElement('iframe');
+  iframe.src = 'iframe.html';
+  document.body.appendChild(iframe);
+  iframe.onload = () => {
+    let iframeDoc = iframe.contentDocument;
+    iframeDoc.write('some text');
+  }
+}
+
+function ej9(){
+  let iframe2 = document.createElement('iframe');
+  iframe2.src = 'iframe.html'; // Asegúrate de que existe
+  document.body.appendChild(iframe2); // se añade el iframe al cuerpo del documento
+  iframe.onload = () => { //cargamos el iframe
+      let iframeDoc = iframe2.contentDocument || iframe2.contentWindow.document;
+      console.log(iframeDoc.body.innerHTML);
+  };
 }
